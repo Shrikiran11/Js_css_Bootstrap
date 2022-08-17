66d4a195a3f1d1aeb029c73f27ce7b5b9a6617ea
Shrikiran11 <92041814+Shrikiran11@users.noreply.github.com>
2022-08-17 14:55:54 +0530

    bubblesort_17_08_22.js

---
+++ bubblesort.js
@@ -0,0 +1,22 @@
+
+function bubbleSort(arr){
+
+    //Outer pass
+    for(let i = 0; i < arr.length; i++){
+
+        //Inner pass
+        for(let j = 0; j < arr.length - i - 1; j++){
+
+            //Value comparison using ascending order
+
+            if(arr[j + 1] < arr[j]){
+
+                //Swapping
+                [arr[j + 1],arr[j]] = [arr[j],arr[j + 1]]
+            }
+        }
+    };
+    return arr;
+};
+const arr=[5,3,8,4,6];
+console.log(bubbleSort(arr));
