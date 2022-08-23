374176063c5361468f428c340916ea41cc31760f
Shrikiran11 <92041814+Shrikiran11@users.noreply.github.com>
2022-08-23 10:16:33 +0530

    AddTillSingle_20_08_2022
    add all the digits of a number till single digit

--- AddNumTillSingleDigit.java
+++ AddNumTillSingleDigit.java
@@ -1,19 +1,27 @@
 import java.util.Scanner;
-class AddNumTillSingleDigit
-{
-	public static void main(String[] args)
-	{
-		Scanner scn=new Scanner(System.in);
-		int n=scn.nextInt();
-		int sum=0;
-		while(n>0||sum>9){
-			if(n==0){
-				n=sum;
-				sum=0;
+
+class AddNumTillSingleDigit {
+	AddNumTillSingleDigit() {
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Enter number to convert:");
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int sum = 0;
+		if (n > 0 && n < 5000) {
+			while (n > 0 || sum > 9) {
+				if (n == 0) {
+					n = sum;
+					sum = 0;
 				}
-			sum=sum+n%10;
-			n=n/10;
+				sum = sum + n % 10;
+				n = n / 10;
+			}
+			System.out.println(sum);
+		} else {
+			System.out.println("Invalid Input");
 		}
-		System.out.println(sum);
 	}
 }
